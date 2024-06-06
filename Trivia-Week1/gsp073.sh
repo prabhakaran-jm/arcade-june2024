744d3f9f3e5941385aad842e2ee2223b8518d733
Prabhakaran Jayaraman Masani <52810378+prabhakaran-jm@users.noreply.github.com>
2024-06-06 21:20:18 +0100

    Updated gsp073.sh

--- Trivia-Week1/gsp073.sh
+++ Trivia-Week1/gsp073.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export REGION=europe-west1
 gsutil mb -l $REGION -c Standard gs://$DEVSHELL_PROJECT_ID
 curl -o kitten.png https://cdn.qwiklabs.com/8tnHNHkj30vDqnzokQ%2FcKrxmOLoxgfaswd9nuZkEjd8%3D
 gsutil cp kitten.png gs://$DEVSHELL_PROJECT_ID/kitten.png
