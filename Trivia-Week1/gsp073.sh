e3bbcd3c2890af231c63a840b77bb60e47dbbd43
Prabhakaran Jayaraman Masani <prabhakaran.jm@gmail.com>
2024-06-06 21:13:14 +0100

    added solutions for trivia week1

---
+++ Trivia-Week1/gsp073.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export REGION=europe-west1
+gsutil mb -l $REGION -c Standard gs://$DEVSHELL_PROJECT_ID
+curl -o kitten.png https://cdn.qwiklabs.com/8tnHNHkj30vDqnzokQ%2FcKrxmOLoxgfaswd9nuZkEjd8%3D
+gsutil cp kitten.png gs://$DEVSHELL_PROJECT_ID/kitten.png
+gsutil iam ch allUsers:objectViewer gs://$DEVSHELL_PROJECT_ID
