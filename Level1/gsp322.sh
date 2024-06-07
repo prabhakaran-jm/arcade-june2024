cbd92df022cd5ed20f15cc0ac0a3130c54a54655
Prabhakaran Jayaraman Masani <prabhakaran.jm@gmail.com>
2024-06-07 12:45:06 +0100

    added lab instructions for GSP322

--- Level1/gsp322.sh
+++ Level1/gsp322.sh
@@ -2,7 +2,7 @@
 gcloud compute firewall-rules delete open-access --quiet
 gcloud compute instances add-tags bastion --tags=$SSH_IAP_Network_tag --zone=$ZONE
 gcloud compute instances start bastion --zone=$ZONE
-gcloud compute firewall-rules create cloudhustlers \
+gcloud compute firewall-rules create gsp322 \
     --action=ALLOW \
     --network=acme-vpc \
     --rules=tcp:22 \
@@ -10,20 +10,18 @@ gcloud compute firewall-rules create cloudhustlers \
     --target-tags=$SSH_IAP_Network_tag \
     --description="Allow SSH from IAP service" 
 gcloud compute instances add-tags bastion --tags=$SSH_IAP_Network_tag --zone=$ZONE
-gcloud compute firewall-rules create hustlercloud \
+gcloud compute firewall-rules create gsp3221 \
     --action=ALLOW \
     --network=acme-vpc \
     --rules=tcp:80 \
     --source-ranges=0.0.0.0/0 \
-    --target-tags=$HTTP_Network_Tag \
-    --description="Hustlercloud"
+    --target-tags=$HTTP_Network_Tag 
 gcloud compute instances add-tags juice-shop --tags=$HTTP_Network_Tag --zone=$ZONE
-gcloud compute firewall-rules create cloudhustler \
+gcloud compute firewall-rules create gsp3222 \
     --action=ALLOW \
     --network=acme-vpc \
     --rules=tcp:22 \
     --source-ranges=192.168.10.0/24 \
-    --target-tags=$SSH_Internal_Network_tag \
-    --description="Hustlercloud"
+    --target-tags=$SSH_Internal_Network_tag 
 gcloud compute instances add-tags juice-shop --tags=$SSH_Internal_Network_tag --zone=$ZONE
 gcloud compute ssh bastion --zone=$ZONE --quiet
