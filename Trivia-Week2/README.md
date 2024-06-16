050710a8d06f90ec8a785d10966a4e4e377d17df
Prabhakaran Jayaraman Masani <prabhakaran.jm@gmail.com>
2024-06-16 11:21:24 +0100

    added Lab instructions for Trivia Week 2 Labs

---
+++ Trivia-Week2/README.md
@@ -0,0 +1,45 @@
+# The Arcade Trivia June 2024 Week 2
+
+### Trivia Questions  
+
+Which API allows developers to programmatically detect and identify sensitive data like personally identifiable information (PII) within unstructured data streams?  
+Select ONE answer that would be relevant
+**Data Loss Prevention API** 
+Sensitive Data Detect API  
+Data Stream Detect API  
+PII detection API  
+
+Which API can automatically redact sensitive data from text files instead of giving you a list of findings?  
+Select ONE answer that would be relevant  
+Redact API  
+**Data Loss Prevention API**  
+Find API  
+Text Find API  
+
+Which command will you use to create a new virtual machine instance via the command line?  
+Select ONE answer that would be relevant  
+gcp compute instances create  
+**gcloud compute instances create**   
+gcp vm instances create  
+vm instances create  
+
+Which command will you use to create a new persistent disk on the cloud shell command line?  
+Select ONE answer that would be relevant  
+**gcloud compute disks create** 
+compute disks create  
+gcp disks create   
+gcp compute disks create  
+
+Which network tier provides optimal performance for applications that require low latency and high throughput?  
+Select ONE answer that would be relevant  
+VIP Tier  
+**Premium Tier**  
+Standard Tier  
+Best Tier  
+
+Which network tier offers the most cost-effective option for dedicated, long-running workloads with predictable traffic patterns?  
+Select ONE answer that would be relevant  
+Traffic optimize tier  
+**Standard Tier**  
+Cost-effective tier  
+Worloads Tier  
---
+++ Trivia-Week2/gsp107.sh
@@ -0,0 +1,71 @@
+export PROJECT_ID=$DEVSHELL_PROJECT_ID
+
+cat > inspect-request.json <<EOF_END
+{
+  "item":{
+    "value":"My phone number is (206) 555-0123."
+  },
+  "inspectConfig":{
+    "infoTypes":[
+      {
+        "name":"PHONE_NUMBER"
+      },
+      {
+        "name":"US_TOLLFREE_PHONE_NUMBER"
+      }
+    ],
+    "minLikelihood":"POSSIBLE",
+    "limits":{
+      "maxFindingsPerItem":0
+    },
+    "includeQuote":true
+  }
+}
+EOF_END
+
+gcloud auth print-access-token
+
+curl -s \
+  -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+  -H "Content-Type: application/json" \
+  https://dlp.googleapis.com/v2/projects/$PROJECT_ID/content:inspect \
+  -d @inspect-request.json -o inspect-output.txt
+
+cat inspect-output.txt
+
+gsutil cp inspect-output.txt gs://$DEVSHELL_PROJECT_ID-bucket
+
+cat > new-inspect-file.json <<EOF_END
+{
+  "item": {
+     "value":"My email is test@gmail.com",
+   },
+   "deidentifyConfig": {
+     "infoTypeTransformations":{
+          "transformations": [
+            {
+              "primitiveTransformation": {
+                "replaceWithInfoTypeConfig": {}
+              }
+            }
+          ]
+        }
+    },
+    "inspectConfig": {
+      "infoTypes": {
+        "name": "EMAIL_ADDRESS"
+      }
+    }
+}
+EOF_END
+
+curl -s \
+  -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+  -H "Content-Type: application/json" \
+  https://dlp.googleapis.com/v2/projects/$PROJECT_ID/content:deidentify \
+  -d @new-inspect-file.json -o redact-output.txt
+
+cat redact-output.txt
+
+gsutil cp redact-output.txt gs://$DEVSHELL_PROJECT_ID-bucket
+
